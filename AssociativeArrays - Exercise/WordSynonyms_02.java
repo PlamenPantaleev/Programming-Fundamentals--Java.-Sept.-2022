a242473fb5aaf9bbd469479320ce2abb5998ed6b
PlamenPantaleev <114143468+PlamenPantaleev@users.noreply.github.com>
2023-12-29 09:00:32 +0200

    Add
    add problem solutions for Associative Arrays - Exercise

---
+++ AssociativeArrays - Exercise/WordSynonyms_02.java
@@ -0,0 +1,32 @@
+package FundamentalsLab.AssociativeArrays;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class WordSynonyms_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        LinkedHashMap<String, ArrayList<String>> words = new LinkedHashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            String word = scanner.nextLine();
+            String synonym = scanner.nextLine();
+
+           if (!words.containsKey(word)) {
+               words.put(word, new ArrayList<>());
+               words.get(word).add(synonym);
+           } else {
+               words.get(word).add(synonym);
+           }
+        }
+        for (Map.Entry<String, ArrayList<String>> entry: words.entrySet()) {
+            System.out.printf("%s - %s%n", entry.getKey(), String.join(", ", entry.getValue()));
+        }
+    }
+}
